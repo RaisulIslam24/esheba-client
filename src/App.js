81842e6e4366aa54ac51632317b2c5b7e5b9f70d
Muhammad Rifat <hrifat450@gmail.com>
2021-07-20 12:04:48 +0600

    navbar done

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import {
   Route
 } from "react-router-dom";
 import Home from "./components/Home/Home";
+import NavBar from "./components/Home/NavBar/NavBar";
 import Login from "./components/Login/Login";
 function App() {
   return (
     <Router>
+      <NavBar />
       <Switch>
           <Route path="/home">
             <Home />
--- src/components/Home/NavBar/NavBar.js
+++ src/components/Home/NavBar/NavBar.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
 
 const NavBar = () => {
     return (
-        <div>
-            <h3>NavBar</h3>
-        </div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Brand href="#">
+            {/* <img
+                src={logo}
+                width="200px"
+                className="d-inline-block align-top"
+                alt="Store 2 Door"
+            /> */}
+            <h3 className="ms-3">E-Sheba</h3>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ms-auto">
+                
+                <Link className="link" to="/home">Home </Link>
+                <Link className="link" to="/services">Services </Link>
+                <Link className="link" to="/blogs">Blogs </Link>
+                <Link className="link" to="/contact">Contact </Link>
+                <Link className="link" to="/about">About </Link>
+                <Link className="link" to="/login">Login </Link>
+                {/* Dynamically change login button to user image */}
+                {/* {
+                    loggedInUser.email ? <Image src={loggedInUser.photo} alt={loggedInUser.name} style={{width:'40px', height:'40px', marginRight:'60px'}} roundedCircle/> : <Link className="link" to="/login"><button id="login-btn">Login</button></Link>
+                } */}
+            </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 };
 
