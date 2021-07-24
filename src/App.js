8181486c76b331e0235322de855d4fb9ef6f732a
Muhammad Rifat <hrifat450@gmail.com>
2021-07-24 20:14:22 +0600

    fix

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
 import NavBar from "./components/Home/NavBar/NavBar";
 import Login from "./components/Login/Login";
@@ -21,7 +22,7 @@ function App() {
             <Home />
           </Route>
         </Switch>
-        
+        <Footer />
     </Router>
   );
 }
