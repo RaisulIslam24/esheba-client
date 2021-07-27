53aed9e8197776587361fa3a4999dc6bf53dc019
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2021-07-27 16:40:36 +0600

    modify testimonial section

--- src/components/Home/Reviews/Reviews.js
+++ src/components/Home/Reviews/Reviews.js
@@ -1,31 +1,47 @@
+
 import React from 'react';
-import Carousel from "react-elastic-carousel";
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import fakeReviewsData from '../../../data/fakeReviewsData.json';
 import Review from './Review/Review';
-
-
-const breakPoints = [
-    { width: 1, itemsToShow: 1 },
-    { width: 550, itemsToShow: 2 },
-    { width: 768, itemsToShow: 3 },
-]
+import './Reviews.css';
 
 const Reviews = () => {
+    //Owl Carousel Settings
+
+    const options = {
+        loop: true,
+        center: true,
+        items: 3,
+        margin: 0,
+        autoplay: true,
+        dots: true,
+        autoplayTimeout: 3500,
+        smartSpeed: 450,
+        nav: false,
+        responsive: {
+            0: { items: 1 },
+            600: { items: 3 },
+            1000: { items: 3 }
+        }
+    };
+
     return (
-        <div className="p-4 mt-5">
-            <h2 className="text-center">Customer Reviews</h2>
-            <div className="mt-4">
-                <Carousel breakPoints={breakPoints}>
-                    <Review />
-                    <Review />
-                    <Review />
-                    <Review />
-                    <Review />
-                    <Review />
-                    <Review />
-                </Carousel>
+        <section id="testimonial" className="testimonials pt-5 pb-5">
+            <div className="container mt-4">
+                <h4 className="text-center">"TESTIMONIALS"</h4>
+                <h3 className="text-center">What our clients are Saying</h3>
+                <div className="row">
+                    <div className="col-md-12">
+                        <OwlCarousel id="customer-testimonial" className="owl-carousel owl-theme" {...options}>
+                            {fakeReviewsData.map((review, index) => <Review review={review} key={index} />)}
+                        </OwlCarousel>
+                    </div>
+                </div>
             </div>
-        </div>
+        </section>
     );
-};
+}
 
 export default Reviews;
