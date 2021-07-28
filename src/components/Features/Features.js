0e7273ec802b83b09afec0f30191893f46a414b2
Faisal-Ahmed-Tonoy <71918750+Faisal-Ahmed-Tonoy@users.noreply.github.com>
2021-07-28 09:54:52 +0600

    services

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -2,6 +2,13 @@ import React, { useState } from 'react';
 import { useEffect } from 'react';
 import featuresData from'../../data/data.json'
 import FeaturesDetails from '../FeaturesDetails/FeaturesDetails';
+import './Features.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
 
 
@@ -12,11 +19,18 @@ const Features = () => {
         console.log(featuresData);
     },[])
     return (
-        <div className="row  " >
+        <div className="row mx-3 all-services" >
             <h1 style={{textAlign:'center'}}>Services</h1>
+             
          {
-            features.map(feature => <FeaturesDetails feature={feature}></FeaturesDetails>) 
+            features?.slice(0,3).map(feature => <FeaturesDetails feature={feature}></FeaturesDetails>) 
          }
+       <Link to="/services">
+       <div class="d-grid gap-2 col-4 my-5 mx-auto">
+  <button class="btn btn-primary " type="button">Explore Services</button>
+   
+</div>
+       </Link>
         </div>
     );
 };
