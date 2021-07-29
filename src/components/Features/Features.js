32ccb37b4c525538ecdd9c33ed4bc17e56ef4f40
Muhammad Rifat <hrifat450@gmail.com>
2021-07-29 14:41:20 +0600

    fix services part

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -1,22 +1,24 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
-import featuresData from'../../data/data.json'
+import featuresData from '../../data/data.json'
 import FeaturesDetails from '../FeaturesDetails/FeaturesDetails';
 
 
 
 const Features = () => {
-    const [features,setFeatures]= useState([]); 
-    useEffect(() =>{
+    const [features, setFeatures] = useState([]);
+    useEffect(() => {
         setFeatures(featuresData);
         console.log(featuresData);
-    },[])
+    }, [])
     return (
-        <div className="row  mt-5" >
-            <h1 style={{textAlign:'center'}}>Services</h1>
-         {
-            features.map(feature => <FeaturesDetails feature={feature}></FeaturesDetails>) 
-         }
+        <div className="container-fluid">
+            <div className="row mt-5" >
+                <h1 style={{ textAlign: 'center' }}>Services</h1>
+                {
+                    features.map(feature => <FeaturesDetails feature={feature}></FeaturesDetails>)
+                }
+            </div>
         </div>
     );
 };
