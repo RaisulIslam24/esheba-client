ca3e0b2cc06a2641b6742e821714a6e6a2bf5431
Muhammad Rifat <hrifat450@gmail.com>
2021-08-01 10:31:30 +0600

    create chat with us

---
+++ src/components/ChatWithUs/ChatWithUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+
+const ChatWithUs = () => {
+    return (
+        <div>
+            <div><FontAwesomeIcon icon={faPhone} /></div>
+        </div>
+    );
+};
+
+export default ChatWithUs;
