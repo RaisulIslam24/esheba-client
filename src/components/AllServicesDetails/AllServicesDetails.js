b5d9e821268528eaceeedaa5b0370c66d8d43547
Faisal-Ahmed-Tonoy <71918750+Faisal-Ahmed-Tonoy@users.noreply.github.com>
2021-08-12 18:05:33 +0600

    dashboard and shipment

--- src/components/AllServicesDetails/AllServicesDetails.js
+++ src/components/AllServicesDetails/AllServicesDetails.js
@@ -12,11 +12,11 @@ const AllServicesDetails = (props) => {
 
     return (
         <div  className="col-md-4 mt-3 services-details">
-        <Link to={`/services/${id}`}>
+        <Link to={`/services/${id}`} style={{textDecoration:'none'}}>
         <div className="card  img-card my-3 mx-2">
         <img src={image} className="card-img-top " alt="..."style={{height:'24rem'}} />
             <div className="card-body body-bg">
-              <h5 className="card-title">{name}</h5>
+            <h4 className="card-title" style={{fontWeight:'600',textAlign:'center'}}>{name}</h4>
             </div>
            </div>
         </Link>
