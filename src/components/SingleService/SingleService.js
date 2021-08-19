663cd7ab1fa359919fce73531f24544ff520be78
Raisul Islam Nibir <raisulislam.cse3.bu@gmail.com>
2021-08-19 13:20:26 +0600

    Cards updated

--- src/components/SingleService/SingleService.js
+++ src/components/SingleService/SingleService.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 import './SingleService.css';
 
 const SingleService = (props) => {
@@ -13,6 +15,15 @@ const SingleService = (props) => {
           <div className="card-body text-center text-dark">
             <h3 className="card-title" style={{ fontWeight: '600' }}>{serviceName}</h3>
             <h4 className="card-title" style={{ fontWeight: '600' }}>Price: ${price}</h4>
+            <div className="fa-icon d-flex justify-content-center">
+              <FontAwesomeIcon className="fas" icon={faStar} /> <br />
+              <FontAwesomeIcon className="fas" icon={faStar} /> <br />
+              <FontAwesomeIcon className="fas" icon={faStar} /> <br />
+              <FontAwesomeIcon className="fas" icon={faStar} /> <br />
+              <FontAwesomeIcon className="fas Fa" icon={faStar} /> <br />
+              <span className="text-center feedback">(1242 Feedback)</span>
+            </div>
+            <small className="mb-3 courtesy">Courtesy - <span className="sheba">E-Sheba</span></small>
           </div>
         </div>
       </Link>
